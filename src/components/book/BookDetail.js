d33cb06b95385747f08b4108c7d0d352c4c3b793
Alejae1998 <alejagr1998@gmail.com>
2023-01-23 15:27:20 -0800

    destructured the id, used useParams added the link to the bookList.js

--- src/components/book/BookDetail.js
+++ src/components/book/BookDetail.js
@@ -1,9 +1,11 @@
 import Book from './Book';
 import { useBookDetail } from '../../hooks/useBookDetail';
+import { useParams } from 'react-router-dom';
 
 function BookDetail() {
-  const id = 1; // TODO: Use id from route
-  const { book, loading, error } = useBookDetail(id);
+  //   const id = 1; // TODO: Use id from route
+  const { id } = useParams();
+  const { book, loading, error } = useBookDetail({ id });
 
   if (error)
     return (
@@ -14,7 +16,11 @@ function BookDetail() {
 
   if (loading) return <h3>Loading book...</h3>;
 
-  return <Book book={book} showDetail />;
+  return (
+    <>
+      <Book book={book} showDetail />
+    </>
+  );
 }
 
 export default BookDetail;
