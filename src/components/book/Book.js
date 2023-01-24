ea19d151ceb394d913b63d9214d927a962483e12
Alejae1998 <alejagr1998@gmail.com>
2023-01-23 16:05:37 -0800

    delete console.log

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -5,7 +5,6 @@ function Book({
   book: { title, total_pages: totalPages, rating, isbn, published_date: publishedDate, authors },
   showDetail = false,
 }) {
-  console.log(`title`, title);
   return (
     <article className="book">
       <h3>{title}</h3>
